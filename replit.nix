c9d6a0a9a7742d26e1a3d1fc6aee324a9f0ccd8a
ImMALWARE <malwarezz@outlook.com>
2025-04-09 22:58:16 +0300

    ~

--- replit.nix
+++ replit.nix
@@ -1,8 +1,12 @@
 { pkgs }: {
   deps = [
+    pkgs.bash
     pkgs.wireguard-tools
     pkgs.jq
     pkgs.bashInteractive
     pkgs.nodePackages.bash-language-server
+    pkgs.coreutils
+    pkgs.curl
+    pkgs.qrencode
   ];
 }
